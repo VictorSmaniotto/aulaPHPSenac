86ed7aff3e349de5399bfdf4ba4ccf095bb88166
Victor Smaniotto <victorsmaniotto@hotmail.com>
2022-09-22 21:40:36 -0300

    Atividade atrasada, mas concluída

---
+++ atividades/exercicios_aula3/exConcatString.php
@@ -0,0 +1,21 @@
+<?php
+
+$nome = "Victor Smaniotto";
+$cpf = '222.333.444‐33';
+$rg = '34.111.444‐X';
+$cargo = 'Cozinheiro';
+$empresa = 'Senac';
+$cnpj = '51.757.388/0001‐00';
+$endereco = 'R.Paraíba,125 ‐ Marília,SP,17509‐060';
+
+echo "<h1>DECLARAÇÃO DE LOCAL DE TRABALHO</h1>";
+
+echo "<p>Eu, $nome, brasileiro $cargo, inscrito(a) no CPF sob o nº $cpf e no RG nº $rg, declaro que para os devidos fins que possuo vínculo empregatício com a empresa $empresa inscrita no CNPJ sob o nº $cnpj, localizada à $endereco.</p>";
+echo "<br>";
+echo "<p>Por ser expressão da verdade, firmo a presente para efeitos legais.</p>";
+echo "<br>";
+echo "<p>Marília-SP, 15 de Setembro de 2022</p>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo $nome;
