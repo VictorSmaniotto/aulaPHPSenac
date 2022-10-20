e38da8e6601c2197edaac12d4c6cb6ba16c00dd6
Victor Smaniotto <victorsmaniotto@hotmail.com>
2022-10-19 21:54:17 -0300

    Cabeçalho e rodape

---
+++ geradorDeSenhaMVC/view/include/footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <p>Copyright - 2022 | Desenvolvido por <a href="https://github.com/VictorSmaniotto/" style="text-decoration: none;">Victor</a></p>
+</footer>
+</div>
+
+
+</body>
+
+</html>
