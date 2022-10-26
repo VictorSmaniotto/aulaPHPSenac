38ce0ffc855b62ff12357095129bcd42437b6169
Victor Smaniotto <victorsmaniotto@hotmail.com>
2022-10-25 22:10:46 -0300

    Fiz a validação de CNPJ e modifiquei o método calculaMódulo11

--- poo/02_Documento.php
+++ poo/02_Documento.php
@@ -8,15 +8,37 @@ abstract class Documento
 
     public function setNumero($numero)
     {
-        $this-> numero = preg_replace('/[^0-9]/is','',$numero);
+        $this->numero = preg_replace('/[^0-9]/is', '', $numero);
     }
 
     public function getNumero()
     {
         return $this->numero;
     }
-    
-    
+
+    public function calculaModulo11($qtdNumeros, $peso)
+    {
+        $digito = 0;
+        $somatorio = 0;
+        $documento = substr($this->getNumero(), 0, $qtdNumeros);
+
+        for ($index = 0; $index < $qtdNumeros; $index++) {
+            $somatorio += $documento[$index] * $peso--;
+            if($peso < 2)
+            {
+                $peso = 9;
+            }
+        }
+
+        $modulo11 = $somatorio % 11;
+        $digito = 11 - $modulo11;
+
+        if ($digito > 9) {
+            $digito = 0;
+        }
+
+        return $digito;
+    }
 }
 
 class CPF extends Documento
@@ -24,71 +46,31 @@ class CPF extends Documento
 
     public function __construct(string $numero)
     {
-       $this->setNumero($numero);
-        if(strlen($this->getNumero()) !=11)
-        {
-
+        $this->setNumero($numero);
+        if (strlen($this->getNumero()) != 11) {
+            return false;
         }
-        
-
     }
     public function eValido()
     {
-       $digitoX = 0;
-       $somatorioX = 0;
-       $cpfX = substr($this->getNumero(),0,9);
-       $peso = 10;
+        $digitoX = $this->calculaModulo11(9, 10);
+        $digitoY = $this->calculaModulo11(10, 11);
 
-    //    $somatorioX += ($cpfX[0] * 10);
-    //    $somatorioX += ($cpfX[1] * 9);
-    //    $somatorioX += ($cpfX[2] * 8);
-    //    $somatorioX += ($cpfX[3] * 7);
-    //    $somatorioX += ($cpfX[4] * 6);
-    //    $somatorioX += ($cpfX[5] * 5);
-    //    $somatorioX += ($cpfX[6] * 4);
-    //    $somatorioX += ($cpfX[7] * 3);
-    //    $somatorioX += ($cpfX[8] * 2);
-        
-    //    echo $somatorioX;
+        $cpfCalculado = substr($this->getNumero(), 0, 9);
+        $cpfCalculado = $cpfCalculado . $digitoX . $digitoY;
 
-       for($index = 0;$index < 9; $index++)
-       {
-            $somatorioX += $cpfX[$index] * $peso--;
-            // $peso--;
-            
-       }
-       echo $somatorioX;
-
-       $modulo11 = $somatorioX % 11;
-       echo "<br>" . $modulo11;
-
-       $digitoX = 11 - $modulo11;
-       echo "<br>" . $digitoX;
-
-       if($digitoX > 9){
-        $digitoX = 0;
-    }
-        echo "<br>" . $digitoX;
-
-      
-       
-      
-        // if(strlen($this->getNumero()) !=11)
-        // {
-        //     return false;
-        // }
-        // if(preg_match('/(\d)\1{10}/', $this->getNumero()))
-        // {
-        //     return false;
-        // }
-
+        if ($this->getNumero() == $cpfCalculado) {
+            return true;
+        } else {
+            return false;
+        }
     }
     public function formata()
     {
-        return substr($this->getNumero(), 0, 3). '.' .
-               substr($this->getNumero(), 0, 3). '.' .
-               substr($this->getNumero(), 0, 3). '-' .
-               substr($this->getNumero(), 0, 2);
+        return substr($this->getNumero(), 0, 3) . '.' .
+            substr($this->getNumero(), 3, 3) . '.' .
+            substr($this->getNumero(), 6, 3) . '-' .
+            substr($this->getNumero(), 9, 2);
     }
 }
 
@@ -98,16 +80,34 @@ class CNPJ extends Documento
 
     public function __construct(string $numero)
     {
-        $this-> setNumero($numero);
-        
+        $this->setNumero($numero);
+        if (strlen($this->getNumero()) != 14) {
+            return false;
+        }
     }
     public function eValido()
     {
 
+
+        $digitoX = $this->calculaModulo11(12, 5);
+        $digitoY = $this -> calculaModulo11(13,6);
+
+        $cnpjCalculado = substr($this->getNumero(), 0, 12);
+        $cnpjCalculado = $cnpjCalculado. $digitoX . $digitoY;
+        if ($this->getNumero() == $cnpjCalculado) {
+            return true;
+        } else {
+            return false;
+        }
+       
     }
     public function formata()
     {
-
+        return substr($this->getNumero(), 0, 2) . '.' .
+            substr($this->getNumero(), 2, 3) . '.' .
+            substr($this->getNumero(), 5, 3) . '/' .
+            substr($this->getNumero(), 8, 4) . '-' .
+            substr($this->getNumero(), 12, 2);
     }
 }
 
@@ -117,59 +117,60 @@ class CNH extends Documento
 
     public function __construct(string $numero, string $categoria)
     {
-        $this-> setNumero($numero);
-        $this-> setCategoria($categoria);
-        
+        $this->setNumero($numero);
+        $this->setCategoria($categoria);
     }
 
     public function setCategoria($categoria)
     {
-        $this-> categoria = preg_replace('/[^A-Z]/','',$categoria);
+        $this->categoria = preg_replace('/[^A-Z]/', '', $categoria);
     }
 
     public function getCategoria()
     {
-        return $this-> categoria;
+        return $this->categoria;
     }
 
     public function eValido()
     {
-
     }
 
     public function formata()
     {
-
-    } 
+    }
 }
 
 
 
 
 $cpf = new CPF('501.479.628-11');
-$cpf->eValido();
-// echo 'CPF: '.$cpf->formata();
-// echo '<br>';
-// $cpf->setNumero('CPF:123.456.789.10' );
-// echo 'CPF: '.$cpf->formata();
-// echo '<br>';
+if ($cpf->eValido()) {
+    echo "CPF Válido";
+} else {
+    echo "CPF Inválido";
+}
 
+echo "<br> CPF: " . $cpf->formata();
 
-// $cnpj = new CNPJ('00.999.888/0025-45');
-// echo 'CNPJ: '.$cnpj->getNumero();
-// echo '<br>';
-// $cnpj ->setNumero('CPNJ: 12.345.678/0099-10');
-// echo 'CNPJ: '.$cnpj->getNumero();
-// echo '<br>';
+echo "<hr>";
 
+$cpfVictor = new CPF('460.699.988-80');
+if ($cpfVictor->eValido()) {
+    echo "CPF Válido";
+} else {
+    echo "CPF Inválido";
+}
 
-// $cnh = new CNH('1472583', 'A1');
-// echo 'CNH: '.$cnh->getNumero() . ' ';
-// echo  $cnh->getCategoria();
-// echo '<br>';
-// $cnh -> setNumero('7894562');
-// $cnh -> setCategoria('AB');
-// echo 'CNH: '. $cnh->getNumero().' ';
-// echo $cnh->getCategoria();
+echo "<br> CPF: " . $cpfVictor->formata();
 
+echo "<hr>";
 
+$cnpj = new CNPJ('03.709.814/0025-65');
+
+echo $cnpj->formata();
+echo "<br>";
+if ($cnpj->eValido()) {
+    echo "CNPJ: Válido";
+} else {
+    echo "CNPJ: Inválido";
+}
